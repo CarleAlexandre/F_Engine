70e713f79ee645aac8222527cd4904a7fd8f64f4
fatmeat <pathofyggdrasil@gmail.com>
2024-08-17 10:42:34 +0200

    finally found a map design i like

--- include/map.hpp
+++ include/map.hpp
@@ -1,11 +1,23 @@
 #ifndef MAP_HPP
 # define MAP_HPP
 
+#include <vector>
+
+typedef struct s_chunk {
+	int terrain[16][16];
+	int structure[16][16];
+	int event[16][16];
+} t_chunk;
+
 class MAP {
 	private:
 		int textureID;
-	public:
 
+		std::vector<t_chunk> chunks;
+	public:
+		int getChunk() {
+			return(0);
+		}
 };
 
 #endif
